d614317e551903da364760b3a34005d3ad940454
Willian Fernandess <wFernandes@Smartwatt.local>
2024-04-03 14:58:02 +0100

    asd

--- src/components/cardProjects/CardProjects.jsx
+++ src/components/cardProjects/CardProjects.jsx
@@ -1,20 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useProfile } from '../../context/ProfileContext';
 import './cardProjects.css';
 
 function CardProjects() {
     const { profile } = useProfile();
-    const [loadingImages, setLoadingImages] = useState(true);
-
-    useEffect(() => {
-        const loadImages = async () => {
-            // Simulação de um atraso para carregar as imagens
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            setLoadingImages(false);
-        };
-
-        loadImages();
-    }, []);
 
     if (!profile?.projects || profile.projects.length === 0) {
         return <div> <p>Nenhum Projeto no Momento.</p></div>;
@@ -38,7 +27,7 @@ function CardProjects() {
                             </a>
                         </div>
                         <div className="image">
-                            {!loadingImages && project.img && <img src={project.img} alt={`Project: ${project.title}`} />}
+                            {project.img && <img src={project.img} alt={`Project: ${project.title}`} />}
                         </div>
                     </li>
                 </ul>
@@ -46,5 +35,7 @@ function CardProjects() {
         </div>
     );
 }
+
+
 
 export default CardProjects;
