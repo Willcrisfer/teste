1187583753788403a412bbe914268c0c21b7407b
Willian Fernandess <wFernandes@Smartwatt.local>
2024-04-04 11:50:59 +0100

    updates

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardProjects from "../../components/cardProjects/CardProjects";
 import { useProfile } from "../../context/ProfileContext";
 import background from "../../assets/homePage/profile.png";
+import background2 from "../../assets//projectsPage/drums.png";
 
 
 const Projects = () => {
@@ -15,6 +16,7 @@ const Projects = () => {
     return (
         <section id="projects" className="projects max-width">
             <img src={background} className="projects__background" alt=""/>
+            <img src={background2} className="projects__background2" alt="" />
             
             <div className="projects__content">
                 <h2 className="secondary-title">{profile?.projectsTitle || "Projetos"}</h2>
